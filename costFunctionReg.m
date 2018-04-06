6677fe8b7207ce846791693fc76f64a9eca8e1d3
shrisha <coolshrisha@gmail.com>
2018-04-06 22:59:02 +0530

    two

---
+++ costFunctionReg.m
@@ -0,0 +1,48 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+n = size(X,2);
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+M = 1/m;
+M1 = 2*m;
+L = eye(n);
+L(1,1) = 0;
+
+h = sigmoid(X*theta);
+y_one = -y'*log(h);
+y_zeros = (1.-y)'*log(1.-h);
+
+value = lambda/M1;
+theta_L = L*theta;
+theta_square = theta_L.^2;
+sumof = sum(theta_square);
+regularizing_term = value.*sumof;
+J_1 = M.*(y_one - y_zeros);
+J = J_1 + regularizing_term;
+
+value1 = lambda/m;
+regularizing_term_grad = value1.*theta_L;
+hypo = h.-y;
+hypo1 = X'*hypo;
+grad = M.*hypo1 + regularizing_term_grad;
+
+
+
+
+
+% =============================================================
+
+end
---
+++ index.m
@@ -0,0 +1,56 @@
+%data
+data = csvread('churn_test.csv');
+X = data(:, [2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]); y = data(:, 21);
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 1
+lambda = 1;
+[cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 10
+lambda = 10;
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+[theta, J, exit_flag] = fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
+
+% Compute accuracy on our training set
+p = predict(theta, X);
+fprintf('At lambda value: %f\n',lambda);
+fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
+
+pause;
+
+clear all;
+
+%data
+data = csvread('churn_test.csv');
+X = data(:, [2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]); y = data(:, 21);
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 1
+lambda = 0.1;
+[cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 10
+lambda = 0.1;
+
+% Set Options
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+[theta, J, exit_flag] = fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
+
+
+% Compute accuracy on our training set
+p = predict(theta, X);
+fprintf('At lambda value: %f\n',lambda);
+fprintf('Test Accuracy: %f\n', mean(double(p == y)) * 100);

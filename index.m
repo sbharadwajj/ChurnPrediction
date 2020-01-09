d6fdf05ffa931d102e61dd99512abc4c50a6e71d
Shrisha Bharadwaj <coolshrisha@gmail.com>
2020-01-09 07:54:17 +0100

    typo

--- index.m
+++ index.m
@@ -1,5 +1,5 @@
 %data
-data = csvread('churn_test.csv');
+data = csvread('churn_train.csv');
 X = data(:, [2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]); y = data(:, 21);
 
 % Initialize fitting parameters
